2571bd7a3328ee313d2fb1b2888603626b75e376
iachieve <mhalim80@hotmail.com>
2020-11-05 14:16:30 -0800

    Subarray Sum Equals K

--- [560]Subarray Sum Equals K.java
+++ [560]Subarray Sum Equals K.java
@@ -24,20 +24,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         // from intellij
+        // 2-30
         public int subarraySum(int[] nums, int k) {
             int count = 0;
-            int[] sums = new int[nums.length+1];
+            int sums[] = new int[nums.length + 1];
             sums[0] = 0;
             for (int i = 1; i <= nums.length; i++) {
-                sums[i] = sums[i-1] + nums[i-1];
+                sums[i] = nums[i-1] + sums[i-1];
             }
             for (int start = 0; start < nums.length; start++) {
-                for (int end = start+1; end <=nums.length; end++) {
+                for (int end = start + 1; end <= nums.length ; end++) {
                     if(sums[end] - sums[start] == k) count++;
                 }
             }
-
-            return count;
+            return  count;
         }
     }
 
